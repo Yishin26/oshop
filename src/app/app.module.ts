204a44c80f89cdeb31a8ff49102adc83144df895
Yishin26 <yishinlin@outlook.com>
2018-04-12 17:56:27 +0800

    Fix Navbar and add ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 import {RouterModule} from '@angular/router';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
@@ -39,12 +39,14 @@ import { LoginComponent } from "./login/login.component";
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot([
       {path:'',component:HomeComponent},
       {path:'products',component:ProductsComponent},
       {path:'shopping-cart',component:ShoppingCartComponent},
       { path: 'check-out', component: CheckOutComponent },
       { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'my/orders', component: MyOrdersComponent },
       { path: 'login', component:LoginComponent },
       { path: 'admin/products', component: AdminProductsComponent },
       { path: 'admin/orders', component: AdminOrdersComponent },
