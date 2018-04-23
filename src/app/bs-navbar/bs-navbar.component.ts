7d839c4e457e4013063f8566ea3a86fc0813533c
Yishin26 <yishinlin@outlook.com>
2018-04-23 11:54:56 +0800

    Authentication
    Using google account to apply authentication and authorization by Firebase.

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,23 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(public auth: AuthService) {
+
+  }
+
+  logout() {
+    this.auth.logout();
   }
 
 }
