268b46aa8e318a7e475a00fcff037b230cd8e89b
Yishin26 <yishinlin@outlook.com>
2018-04-23 16:03:44 +0800

    Fix router directer

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -5,13 +5,14 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AuthGuardService implements CanActivate{
 
-  constructor(private auth:AuthService,private router:Router) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate(route, state:RouterStateSnapshot){
-    return this.auth.user$.map(user=>{
-      if(user) return true;
-      this.router.navigate(['/login'],{queryParams:{ returnUrl: state.url}});
+  canActivate(route, state: RouterStateSnapshot) {
+    return this.auth.user$.map(user => {
+      if (user) return true;
+
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
       return false;
     });
-  };
+  }
 }
